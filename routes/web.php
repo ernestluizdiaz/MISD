50ed6c5008d8997a1824e0bc4543294957229d2a
ernestluizdiaz <ernestluizdiaz@gmail.com>
2025-01-02 13:24:47 +0800

    Adding User Authentication Features

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FirebaseController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
 
 Route::get('/test-firebase', [FirebaseController::class, 'testConnection']);
