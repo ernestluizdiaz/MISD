8911d6f00215da64a619a9b90845e0db94baf54b
ernestluizdiaz <ernestluizdiaz@gmail.com>
2025-01-06 03:49:16 +0800

    fix ui of dashboard and table

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,11 +10,15 @@
 </head>
 
 <body>
-  <h1>Welcome to the Dashboard!</h1>
-  <form action="{{ route('logout') }}" method="POST">
-    @csrf
-    <button type="submit">Logout</button>
-  </form>
+  <div class="flex justify-between items-center p-4 ">
+    <h1 class="text-xl font-bold  text-gray-900 md:text-2xl dark:text-white">Welcome to the
+      Dashboard</h1>
+    <form action="{{ route('logout') }}" method="POST">
+      @csrf
+      <button type="submit" class="text-white bg-red-600 rounded-md text-sm p-2">Logout</button>
+    </form>
+  </div>
+
 
   @include('table', ['tickets' => $tickets])
 </body>
